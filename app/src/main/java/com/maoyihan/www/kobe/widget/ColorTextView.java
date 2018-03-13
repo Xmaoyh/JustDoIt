50b8670959d94c250407b28f13d3b8e61f12f828
Xmaoyh <353804616@qq.com>
2018-03-13 16:10:42 +0800

    向左向右变色

--- app/src/main/java/com/maoyihan/www/kobe/widget/ColorTextView.java
+++ app/src/main/java/com/maoyihan/www/kobe/widget/ColorTextView.java
@@ -19,11 +19,11 @@ public class ColorTextView extends android.support.v7.widget.AppCompatTextView {
     /**
      * 左边的字体颜色的画笔
      */
-    private Paint mLeftPaint;
+    private Paint mOriginPaint;
     /**
      * 右边的字体颜色的画笔
      */
-    private Paint mRightPaint;
+    private Paint mChangedPaint;
     /**
      * 当前的进度
      */
@@ -45,8 +45,8 @@ public class ColorTextView extends android.support.v7.widget.AppCompatTextView {
     }
 
     private void initPaint() {
-        mLeftPaint = getPaintByColor(Color.RED);
-        mRightPaint = getPaintByColor(Color.BLACK);
+        mOriginPaint = getPaintByColor(Color.BLACK);
+        mChangedPaint = getPaintByColor(Color.RED);
     }
 
     private Paint getPaintByColor(int color) {
@@ -64,43 +64,44 @@ public class ColorTextView extends android.support.v7.widget.AppCompatTextView {
         int width = getWidth();
         if (!TextUtils.isEmpty(mText)) {
             int middle = (int) (mCurrentProgress * width);
-            drawLeft(canvas, middle);
-            drawRight(canvas, middle);
+            drawOrigin(canvas, middle);
+            drawChanged(canvas, middle);
         }
     }
 
-    private void drawLeft(Canvas canvas, int middle) {
-//        drawText(canvas, 0, middle, mLeftPaint); //向左变色
-        drawText(canvas, 0, getWidth()-middle, mLeftPaint);//向右变色
+    private void drawOrigin(Canvas canvas, int middle) {
+//        drawText(canvas, middle, getWidth(), mOriginPaint); //向右变色
+        drawText(canvas, 0, getWidth() - middle, mOriginPaint);//向左变色
     }
 
-    private void drawRight(Canvas canvas, int middle) {
-//        drawText(canvas, middle, getWidth(), mRightPaint);//向左变色
-        drawText(canvas, getWidth()-middle, getWidth(), mRightPaint);//向右变色
+    private void drawChanged(Canvas canvas, int middle) {
+        //向左还是向右依照的是画Changed的起点，0则向左开始，getWidth() - middle则向右开始
+//        drawText(canvas, 0, middle, mChangedPaint);//向右变色
+        drawText(canvas, getWidth() - middle, getWidth(), mChangedPaint);//向左变色
     }
 
-    private void drawText(Canvas canvas, int startX, int endX, Paint paint){
+    private void drawText(Canvas canvas, int startX, int endX, Paint paint) {
         canvas.save();
         // 截取绘制的内容，待会就只会绘制clipRect设置的参数部分
         canvas.clipRect(startX, 0, endX, getHeight());
         // 获取文字的范围
         Rect bounds = new Rect();
-        mLeftPaint.getTextBounds(mText, 0, mText.length(), bounds);
+        mOriginPaint.getTextBounds(mText, 0, mText.length(), bounds);
         // 获取文字的Metrics 用来计算基线
-        Paint.FontMetricsInt fontMetrics = mLeftPaint.getFontMetricsInt();
+        Paint.FontMetricsInt fontMetrics = mOriginPaint.getFontMetricsInt();
         // 获取文字的宽高
         int fontTotalHeight = fontMetrics.bottom - fontMetrics.top;
         // 计算基线到中心点的位置
         int offY = fontTotalHeight / 2 - fontMetrics.bottom;
         // 计算基线位置
-        int baselineX = (getWidth() - bounds.width()) / 2 ;
-        int baselineY = (getHeight() + fontTotalHeight) / 2 - offY ;
+        int baselineX = (getWidth() - bounds.width()) / 2;
+        int baselineY = (getHeight() + fontTotalHeight) / 2 - offY;
         canvas.drawText(mText, baselineX, baselineY, paint);
         canvas.restore();
     }
 
-    private void performAnimation(){
-        ValueAnimator valueAnimator = ValueAnimator.ofFloat(0,1);
+    private void performAnimation() {
+        ValueAnimator valueAnimator = ValueAnimator.ofFloat(0, 1);
         valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
             public void onAnimationUpdate(ValueAnimator animation) {
